fb7c56b716e3a896a2d49e0685fe1233febadda6
akagi <dev.akagi@gmail.com>
2023-05-29 14:59:46 +0000

    goのgithub自動テスト

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World!!")
+		c.String(http.StatusOK, "こんにちは")
 	})
 
 	r.Run()
---
+++ main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHelloWorld(t *testing.T) {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "こんにちは")
+	})
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "こんにちは", w.Body.String())
+}
